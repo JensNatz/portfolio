4e5726ea9eea973ada37a5528aae282a44f23d34
Jens Natzschka <jens.natzschka@posteo.de>
2024-11-18 09:02:24 +0100

    finished mobile menu

--- src/app/main-page/components/hero-section/navbar/navbar.component.ts
+++ src/app/main-page/components/hero-section/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BurgerIconComponent } from "../../../../shared/components/burger-icon/burger-icon.component";
 import { MobileNavmenuComponent } from '../../../../shared/components/mobile-navmenu/mobile-navmenu.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [BurgerIconComponent, MobileNavmenuComponent],
+  imports: [CommonModule, BurgerIconComponent, MobileNavmenuComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
