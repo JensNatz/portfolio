3970a7a0d7c4fc9833467de80777e770b1692217
Jens Natzschka <jens.natzschka@posteo.de>
2024-11-21 09:03:49 +0100

    minor style fixes

--- src/app/main-page/components/hero-section/navbar/navbar.component.ts
+++ src/app/main-page/components/hero-section/navbar/navbar.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { BurgerIconComponent } from "../../../../shared/components/burger-icon/burger-icon.component";
 import { MobileNavmenuComponent } from '../../../../shared/components/mobile-navmenu/mobile-navmenu.component';
 import { LanguageSwitchComponent } from '../../../../shared/components/language-switch/language-switch.component';
+import {TranslatePipe} from "@ngx-translate/core";
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, BurgerIconComponent, MobileNavmenuComponent, LanguageSwitchComponent],
+  imports: [CommonModule, BurgerIconComponent, MobileNavmenuComponent, LanguageSwitchComponent, TranslatePipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
