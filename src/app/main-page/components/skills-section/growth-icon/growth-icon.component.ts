faf16c88508cad5718a430a6f0b050c90c42bdd0
Jens Natzschka <jens.natzschka@posteo.de>
2024-11-26 12:21:31 +0100

    added translation for credentials

--- src/app/main-page/components/skills-section/growth-icon/growth-icon.component.ts
+++ src/app/main-page/components/skills-section/growth-icon/growth-icon.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-growth-icon',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './growth-icon.component.html',
   styleUrl: './growth-icon.component.scss'
 })
